fbde014a015d12bb9e6f91ef3245d00fcb62712d
Fran <fran.perezheredia@gmail.com>
2026-01-15 10:03:45 +0100

    Implement Jenkins pipeline for app build and analysis
    Added a Jenkins pipeline for building and analyzing the application.

--- app.py
+++ app.py
@@ -1,54 +1,45 @@
-# Ejercicio de examen. Equipo del Rodrigo caro.
-# Estudiante: Francisco Javier Perez Heredia
+pipeline {
+    agent any
 
-goles = {}
+    tools {
+        // Aquí llamamos a la herramienta que configuramos en el paso 3.3
+        jdk 'Default' // A veces necesario, si falla quítalo
+        scannerHome 'sonar-scanner' 
+    }
 
-enfermeria = ["Rocha", "Batres", "Cupillar"]
-
-while True:
-    #       MENU
-    print ("1.  Anotar goles")
-    print ("2.  Consultar jugador")
-    print ("3.  Informe del equipo")
-    print ("4.  salir")
-    opcion = int(input("Seleccione una opcion: "))
-
-    #   Opcion 1: Anotar goles
-    if opcion == 1:
-        print("Anotar goles...")
-        nombre = input("Nombre del jugador: ")
-        if nombre in enfermeria:
-            print (f"El jugador {nombre} esta lesionado en la enfermeria")
-            continue
-        try:
-            goles_n = int(input("Goles: "))
-        except ValueError:
-            print("Error: Numero de goles en formato invalido")
-            continue
-        else:
-            goles[nombre]=goles_n
-
-    #   Opcion 2: Consultar jugador
-    if opcion == 2:
-        print("Consultar jugador...")
-        jugador=input("Jugador a consultar: ")
-        if jugador not in goles:
-            print("El jugador no existe")
-            continue
-        print (f"El jugador {jugador} tiene {goles[jugador]} goles")
-
-    #   Opcion 3: Mostrar jugadores y total
-    if opcion == 3:
-        print("Jugador  -   Goles")
-        for  nombre, goles_t in goles.items():
-            print(f"{nombre}    -   {goles_t}")
+    stages {
+        stage('Preparar Entorno') {
+            steps {
+                echo 'Iniciando pipeline con SonarQube...'
+            }
+        }
         
-        total = 0
-        for jugador in goles:
-            total = total + goles[jugador]
-        
-        print(f"Total: {total}")
-    
-    #   Opcion 4: Salir
-    if opcion == 4:
-        break
+        // NUEVA ETAPA: Análisis de Código estático
+        stage('Análisis de SonarQube') {
+            steps {
+                // "SonarQube" es el nombre que pusimos en el paso 3.2 (System)
+                withSonarQubeEnv('SonarQube') {
+                    // Ejecutamos el scanner pasando la KEY del proyecto
+                    sh "${scannerHome}/bin/sonar-scanner \
+                        -Dsonar.projectKey=App-Rodrigo-Caro \
+                        -Dsonar.sources=. \
+                        -Dsonar.python.version=3"
+                }
+            }
+        }
+
+        stage('Construir Imagen') {
+            steps {
+                script {
+                    sh 'docker build -t app-rodrigo-caro:${BUILD_ID} .'
+                }
+            }
+        }
+
+        stage('Limpieza') {
+            steps {
+                sh 'docker rmi app-rodrigo-caro:${BUILD_ID}'
+            }
+        }
+    }
+}
